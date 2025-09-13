1756b99d5b44db78ad06975de92b24b32383a3a5
FabricioRubim <fabriciofraga200618@gmail.com>
2025-09-13 14:04:19 -0300

    Arquivos

---
+++ src/Arquivos C/exercicio5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 0;
+
+    while(n < 100){
+        n++;
+        printf("%d\n", n);
+    }
+    
+
+    return 0;
+}
---
+++ src/Arquivos C/math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+
+    do
+    {
+        n = 0;
+        n++;
+        printf("%d", n);
+    } while (n < 5);
+
+    return 0;
+}
